dfca7304eff6d95eb2e228bd6533e1e7db56f9c7
bulldozerat <bulldozerat@gmail.com>
2017-06-23 01:42:02 +0300

    Update index.php

--- log the user in and out/index.php
+++ log the user in and out/index.php
@@ -3,7 +3,9 @@ require 'core.inc.php';
 require 'connect.inc.php';
 
 if (loggedin()) {
+    global $user_id;
     echo 'You are logged in. <a href="logout.php">Log out</a>';
+    get_user_info($_SESSION['user_id']['id']);
 }else{
     include 'loginform.inc.php';
 }
