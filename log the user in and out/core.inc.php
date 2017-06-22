3289e1eb7442459f8d0ac69eff1e434484995622
bulldozerat <bulldozerat@gmail.com>
2017-06-23 01:41:12 +0300

    Update core.inc.php

--- log the user in and out/core.inc.php
+++ log the user in and out/core.inc.php
@@ -1,14 +1,24 @@
 <?php
 ob_start();
 session_start();
+include 'connect.inc.php';
 
 $current_file = $_SERVER['SCRIPT_NAME'];
-$http_referer = $_SERVER['HTTP_REFERER'];//dava stranicata ot koqto idvame
+$http_referer = $_SERVER['HTTP_REFERER'];
 
 function loggedin() {
     if ( isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])) {
         return true;
     }else{
         return false;
+    }
+}
+
+function get_user_info($user_id) {
+    global $conn;
+    $query = "SELECT `firstname`, `surname` FROM `users` WHERE `id` = '$user_id'";
+    if($query_run = mysqli_query($conn, $query)) {
+        $row = mysqli_fetch_assoc($query_run);
+        echo "<br>Hello, " . $row['firstname'] . ' ' . $row['surname'];
     }
 }
